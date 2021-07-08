c0aa9c678e1d17f95c7fb5f180155150d60e49f2
stephull <shullender0907@gmail.com>
2021-07-08 16:56:47 -0700

    start over, add JFrame and JButton

--- Main.java
+++ Main.java
@@ -1,43 +1,18 @@
-/*
- *  Main.java
- *  Main method, responsible for window properties & operations 
- *  (e.g. title, resize, visibility, closing).
- */
-
 import java.util.*;
 import java.io.*;
 import javax.swing.*;
-import javax.swing.event.*;
 import java.awt.*;
-import java.awt.event.*;
 
-public class Main extends JFrame {
-	
-	private Main() {
-		// call contents of window into the main framework/window
-		this.add(new Window());
-		this.setTitle("Minesweeper Game");
-		JFrame.setDefaultLookAndFeelDecorated(true);
-		
-		// test text
-		// JLabel label = new JLabel("Hello world!!!");
-		// label.setFont(label.getFont().deriveFont(24.0f));
-		// this.add(label);
-		
-		// exit the application by closing with the 'X' button or System.exit
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		// unable to resize window
-		this.setResizable(true);
-		
-		// sizes the contents appropriate to the size of the window
-		this.pack();
-		
-		// set to visible
-		this.setVisible(true);
-	}
-	
-	public static void main(String[] args) {
-		new Main();
-	}
+public class Main {
+    public static void main(String[] args) {
+        // create frame
+        JFrame frame = new JFrame("Minesweeper");
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // create button
+        JButton button = new JButton("test button");
+        frame.add(button);
+    }
 }
